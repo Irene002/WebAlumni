1633c068589b22d71eadf857ee41dd1f692b80d3
Astaroth <seanadare@gmail.com>
2025-01-02 10:36:01 +0800

    l

--- src/pages/Mahasiswa.jsx
+++ src/pages/Mahasiswa.jsx
@@ -7,15 +7,17 @@ const Mahasiswa = () => {
 
   return (
     <>
-      <section className="p-32 px-96 bg-orange-100 h-screen">
+      <section className="p-32 px-4 sm:px-24 2xl:px-96 bg-orange-100 h-screen">
         <div className="">
           <div className="flex justify-center text-center mb-12">
           <h2>Angkatan</h2>
           </div>
-          <div className="grid grid-cols-2 gap-8">
-            {DataAngkatan.map((angkatan, index) => (
-              <button style={{ animationDelay: `${index * 0.5}s` }}  onClick={() => Navigate(`/mahasiswa/${angkatan.Angkatan}`)} className="FadeIn bg-white shadow-md rounded-md p-8 border font-bold border-orange-500 hover:bg-orange-500 hover:text-white transition-all duration-300" key={index}>{angkatan.Angkatan}</button>
+          <div className="flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-12">
+            {DataAngkatan.slice(0,2).map((angkatan, index) => (
+              <button style={{ animationDelay: `${index * 0.5}s` }}  onClick={() => Navigate(`/mahasiswa/${angkatan.Angkatan}`)} className="FadeIn w-full max-w-[400px] min-w-[290px] bg-white shadow-md rounded-md p-8 border font-bold border-orange-500 hover:bg-orange-500 hover:text-white transition-all duration-300" key={index}>{angkatan.Angkatan}</button>
             ))}
+          </div>
           </div>
         </div>
       </section>
