5c8743858335e544787c37e413b4608538545523
Astaroth <seanadare@gmail.com>
2025-01-03 00:31:03 +0800

    update

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,32 +5,26 @@ const Home = lazy(() => import('../pages/Home.jsx'))
 const Mahasiswa = lazy(() => import('../pages/Mahasiswa.jsx'))
 const About = lazy(() => import('../pages/About.jsx'))
 const MahasiswaDetail = lazy(() => import('../pages/MahasiswaDetail.jsx'))
-const Angkatan2019 = lazy(() => import('../pages/angkatan/Angkatan2019.jsx'))
-const Angkatan2020 = lazy(() => import('../pages/angkatan/Angkatan2020.jsx'))
-const Angkatan2021 = lazy(() => import('../pages/angkatan/Angkatan2021.jsx'))
-const Angkatan2022 = lazy(() => import('../pages/angkatan/Angkatan2022.jsx'))
+const AngkatanDetail = lazy(() => import('../pages/AngkatanDetail'))
 
 const Routing = () => {
   return (
     <>
-        <Suspense fallback={
-          <div className='w-full flex justify-center items-center inset-0 bg-orange-200 h-screen'>
-            <h1 className='animate-pulse'>Loading...</h1>
-          </div>
-        }>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/mahasiswa' element={<Mahasiswa />} />
-            <Route path='/tentang' element={<About />} />
-            <Route path='/mahasiswa/:angkatan/:name' element={<MahasiswaDetail />} />
+      <Suspense fallback={
+        <div className='w-full flex justify-center items-center inset-0 bg-orange-200 h-screen'>
+          <h1 className='animate-pulse'>Loading...</h1>
+        </div>
+      }>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/mahasiswa' element={<Mahasiswa />} />
+          <Route path='/tentang' element={<About />} />
+          <Route path='/mahasiswa/:angkatan/:name' element={<MahasiswaDetail />} />
 
-            {/* Angkatan Pages */}
-            <Route path='/mahasiswa/2019' element={<Angkatan2019 />} />
-            <Route path='/mahasiswa/2020' element={<Angkatan2020 />} />
-            <Route path='/mahasiswa/2021' element={<Angkatan2021 />} />
-            <Route path='/mahasiswa/2022' element={<Angkatan2022 />} />
-          </Routes>
-        </Suspense>
+          {/* Angkatan Pages */}
+          <Route path='/mahasiswa/:angkatan' element={<AngkatanDetail />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
