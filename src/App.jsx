5a3ab407a33a480676792174d5067c68c30323fc
Astaroth <seanadare@gmail.com>
2025-01-03 03:57:14 +0800

    Removed Overflow Bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MobileNav from './components/MobileNav';
 const App = () => {
   return (
     <>
-    <main className='relative'>
+    <main className='relative overflow-hidden'>
       <NavigationBar />
       <section>
         <Routing />
